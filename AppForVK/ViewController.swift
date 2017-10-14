93ac4e7eac75edcc68dc73dbc96a4a3dfe75c6bd
Спартак Ермолаев <spartakermolaev@MacBook-Pro-Spartak.local>
2017-10-15 00:01:06 +0300

    Initial Commit

---
+++ AppForVK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppForVK
+//
+//  Created by Спартак Ермолаев on 15.10.17.
+//  Copyright © 2017 Ermolaev Spartak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
